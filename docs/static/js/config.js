2c8ae78cbd4eb3b7b3f9f11beea56a679fce4f1b
Vronchloe <167137744+Vronchloe@users.noreply.github.com>
2025-11-09 11:25:19 +0530

    Add configuration for API URL and API key

---
+++ docs/static/js/config.js
@@ -0,0 +1,3 @@
+/* config.js - Change API_URL and API_KEY here */
+const API_URL = 'https://traffic-api-c04h.onrender.com'; // Replace with your Render URL
+const API_KEY = '12345'; // Must match backend API_KEY
